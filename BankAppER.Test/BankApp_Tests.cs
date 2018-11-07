76ca24ccaaed5400f908b7fb9b36b61521661f49
Egidio Ricchiuti <egisan3@yahoo.com>
2018-11-07 13:50:51 +0100

    Main proj and Test proj setup commit

---
+++ BankAppER.Test/BankApp_Tests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Xunit;
+
+namespace BankAppER.Test
+{
+    public class BankApp_Tests
+    {
+        [Fact]
+        public void Test_example()
+        {
+
+
+            Assert.True(true);
+        }
+    }
+}

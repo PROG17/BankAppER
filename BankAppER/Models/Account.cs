22da92122f04c66afe6308036250e015411097a3
Egidio Ricchiuti <egisan3@yahoo.com>
2018-11-07 15:30:18 +0100

    Setup customer, Account and BankrRepo
    Add Customer, Account, BankRepo and IBankRepo

---
+++ BankAppER/Models/Account.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankAppER.Models
+{
+    public class Account
+    {
+        public int Id { get; set; }
+
+        public decimal Total { get; set; }
+    }
+}
--- BankAppER/Startup.cs
+++ BankAppER/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BankAppER.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -24,12 +25,16 @@ namespace BankAppER
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
+            //services.Configure<CookiePolicyOptions>(options =>
+            //{
+            //    // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+            //    options.CheckConsentNeeded = context => true;
+            //    options.MinimumSameSitePolicy = SameSiteMode.None;
+
+
+            //});
+
+            services.AddSingleton<IBankRepository, BankRepository>();
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
